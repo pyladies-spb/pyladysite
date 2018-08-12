df1570963cc4eafcaa0424a5a9bd69c614d74d5e
Dmitry Nazarov <mail@nazarov.tech>
2018-08-12 04:40:38 +0300

    Fix publications page serving

--- app.py
+++ app.py
@@ -1,30 +1,40 @@
-from flask import Flask, request, send_from_directory
-# set the project root directory as the static folder, you can set others.
+from flask import Flask, send_from_directory
+
 app = Flask(__name__, static_url_path='/generated')
 
 generated_site = 'generated/_site'
 
+
 @app.route('/')
 def root():
-    return send_from_directory(generated_site,'index.html')
+    return send_from_directory(generated_site, 'index.html')
+
 
 @app.route('/faq/')
 def faq():
-    return send_from_directory(generated_site,'faq/index.html')
+    return send_from_directory(generated_site, 'faq/index.html')
 
-@app.route('/partners/')
-def partners():
-    return send_from_directory(generated_site,'partners/index.html')
 
 @app.route('/resources/')
 def resources():
-    return send_from_directory(generated_site,'resources/index.html')
+    return send_from_directory(generated_site, 'resources/index.html')
+
 
 @app.route('/CodeOfConduct/')
 def coc():
-    return send_from_directory(generated_site,'CodeOfConduct/index.html')
+    return send_from_directory(generated_site, 'CodeOfConduct/index.html')
+
+
+@app.route('/publications/')
+def partners():
+    return send_from_directory(generated_site, 'publications/index.html')
+
+
+@app.route('/partners/')
+def partners():
+    return send_from_directory(generated_site, 'partners/index.html')
 
 
 @app.route('/assets/<path:path>')
 def assets(path):
-    return send_from_directory(generated_site+'/assets',path)
+    return send_from_directory(generated_site + '/assets', path)
